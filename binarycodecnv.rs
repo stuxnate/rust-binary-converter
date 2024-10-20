53bcc3165c5ba4cc3bee47dc6cee0b09348ea6d7
stuxnate <stuxnate@proton.me>
2024-10-20 03:06:18 -0700

    Added some comment and corrected some spelling errors

--- binarycodecnv.rs
+++ binarycodecnv.rs
@@ -1,11 +1,14 @@
-use std::io::{self, Write};
+use std::io::{self, Write}; //We need to flush the screen to display prompt
 
 fn main(){
-    let mut user_query = String::new();
+    //initialize our variable to store user input
+    let mut user_query = String::new(); 
     println!("This program helps convert entered *numbers to binary");
     print!("Number to Convert: ");
+    //flush screen to allow the prompt above to be displayed
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut user_query).expect("Faild to read input");
+    //converting user input to int or return an exception if the input is not valid i.e if user did not enter an int
+    io::stdin().read_line(&mut user_query).expect("Failed to read input");
     let user_number : i32  = user_query.trim().parse().expect("You need to enter a valid int/number");
     println!("BinaryFormat: -> {:b}", user_number); //Display the int in binary
 }
